02ed5ba63097e1ed9360484077b70203326623e6
Tim Lange <mail@tim-lange.me>
2022-04-02 15:05:37 +0200

    fix option showScanCode

--- src/group-session.js
+++ src/group-session.js
@@ -307,7 +307,7 @@
      * Fullscreen *
      **************/
     const tryInsertFullScreen = () => {
-        if (join_session_token == null) {
+        if (!showScanCode || join_session_token == null) {
             return;
         }
         const main = document.querySelector(".npv-main-container");
